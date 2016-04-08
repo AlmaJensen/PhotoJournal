031a0e7412d5e27c040c076a6ad182c0c709d0b2
AlmaJensen <jazzdevstudio@gmail.com>
2016-04-08 15:23:22 -0600

    Added more Page Stubs
    Added Stubs for more app pages.

--- Journal/Journal/Pages/AddEditPhotoEntryPage.xaml.cs
+++ Journal/Journal/Pages/AddEditPhotoEntryPage.xaml.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Xamarin.Forms;
+
 namespace Journal.Pages
 {
-	class LoginPage
+	public partial class AddEditPhotoEntryPage : ContentPage
 	{
+		public AddEditPhotoEntryPage()
+		{
+			InitializeComponent();
+		}
 	}
 }
