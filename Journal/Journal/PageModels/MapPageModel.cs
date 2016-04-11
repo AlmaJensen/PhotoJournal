14ac07864265b248eafd32e338644e594813d63b
AlmaJensen <jazzdevstudio@gmail.com>
2016-04-11 05:56:31 -0700

    Added backing models
    Added backing models for additional pages.

---
+++ Journal/Journal/PageModels/MapPageModel.cs
@@ -0,0 +1,15 @@
+﻿using FreshMvvm;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journal.PageModels
+{
+    [ImplementPropertyChanged]
+    class MapPageModel : FreshBasePageModel
+    {
+    }
+}
