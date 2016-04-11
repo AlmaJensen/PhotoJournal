1948414d605ca4192610e9e0d1e185bca3fb7748
AlmaJensen <jazzdevstudio@gmail.com>
2016-04-11 07:41:28 -0700

    Pin Page backing
    Added backing for pin pages.

--- Journal/Journal/PageModels/PinPageModel.cs
+++ Journal/Journal/PageModels/PinPageModel.cs
@@ -5,11 +5,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace Journal.PageModels
 {
 	[ImplementPropertyChanged]
 	class LoginPageModel : FreshBasePageModel
 	{
+		public string EntryText {
+			get;
+			set;
+		}
+		public Command LoginCommand {
+			get
+			{
+				return new Command(async x => await CoreMethods.PushPageModel<RegistrationPageModel>());
+			}
+		}
+
 	}
 }
