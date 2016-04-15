496161ef163a9a7a5e067f74f026937ad6c9456d
AlmaJensen <jazzdevstudio@gmail.com>
2016-04-15 07:43:34 -0600

    Entry Model
    Added model for handling entries locally

---
+++ Journal/Journal/DataModels/EntryModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journal.DataModels
+{
+	public class EntryModel
+	{
+
+		public DateTime DateOccurred { get; set; }
+		public string EntryText { get; set; }
+		public string EntryTitle { get; set; }
+		public string EntrySummary { get; set; }
+		public PhotoModel MainImage { get; set; }
+		public List<PhotoModel> Images { get; set; }
+
+	}
+}
