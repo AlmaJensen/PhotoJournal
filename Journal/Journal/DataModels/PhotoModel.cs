e0bad09d009e18ef8ffd4d968bfd8e905664ee7f
AlmaJensen <jazzdevstudio@gmail.com>
2016-04-15 07:43:08 -0600

    Photo model for local photo usage
    Added a new model to be used for displaying photos locally.

---
+++ Journal/Journal/DataModels/PhotoModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Journal.DataModels
+{
+	public class PhotoModel
+	{
+		public string ImageName { get; set; }
+		public string ImageLocation { get; set; }
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
+		public string LocationName { get; set; }
+		public string ImageTitle { get; set; }
+		public string ImageDescription { get; set; }
+	}
+}
